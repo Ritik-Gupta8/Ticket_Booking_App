50f30892cf004f3dad3c023ae3bdc1d6573648df
23dp3000001 <23dp3000001@ds.study.iitm.ac.in>
2025-04-15 19:06:55 +0530

    app chnages

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def setup_app():
     app=Flask(__name__)
     app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///ticket_show.sqlite3" #Having db file
     db.init_app(app) #Flask app connected to db 
-    api.init_app(app) #Flask app coonect to apis
+    # api.init_app(app) #Flask app coonect to apis
     app.app_context().push() # Direct access to other modules
     app.debug=True
     print("Ticker show app is started..")
