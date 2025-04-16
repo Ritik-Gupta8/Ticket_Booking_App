982dfa4c930294574bed0de7ddad5451a6c50f90
eren-yeager08 <eren2000@gmail.com>
2025-04-16 14:28:23 +0530

    new

--- api/app.py
+++ api/app.py
@@ -1,7 +1,10 @@
 # api/app.py
 
 from flask import Flask
-from app import app
+# api/app.py
+
+from app import app as application  # Import the existing Flask app from root and expose it as `application`
+
 
 app = Flask(__name__)
 
