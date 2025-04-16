f92d65c7ef1d2e4c53299f32c98e4e099e238735
eren-yeager08 <eren2000@gmail.com>
2025-04-16 19:12:39 +0530

    new

--- app.py
+++ app.py
@@ -1,23 +1,32 @@
-import os
-os.environ['MPLCONFIGDIR'] = '/tmp/matplotlib'
-
+#Starting of the application
 from flask import Flask
 from backend.models import db
+from backend.api_controllers import *
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ticket_show.sqlite3"
-app.debug = True
 
-# Initialize DB
-db.init_app(app)
+app=None
 
-# Import controllers after app creation
-with app.app_context():
-    from backend.controllers import register_routes
-    register_routes(app)
-    db.create_all()
+def setup_app():
+    app=Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///ticket_show.sqlite3" #Having db file
+    db.init_app(app) #Flask app connected to db 
+    # api.init_app(app) #Flask app coonect to apis
+    app.app_context().push() # Direct access to other modules
+    app.debug=True
+    print("Ticker show app is started..")
 
-print("Ticket show app is started!")
+#call th funnction
+setup_app()  
+#helo
+from backend.controllers import *
 
-if __name__ == "__main__":
+@app.route("/ping")
+def ping():
+    return "Flask app is working!"
+
+# Required for Vercel
+def handler(environ, start_response):
+    return app(environ, start_response)
+
+if __name__ =="__main__":
     app.run()
