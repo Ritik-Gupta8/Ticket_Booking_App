af9c58981019b8c72120e9ad653e901f677b8918
eren-yeager08 <eren2000@gmail.com>
2025-04-16 19:13:49 +0530

    new

--- api/app.py
+++
@@ -1,9 +0,0 @@
-from app import app
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
-
-# Vercel-compatible WSGI application
-application = DispatcherMiddleware(app)
-
-def handler(event, context):
-    with app.app_context():
-        return application(event, context)
